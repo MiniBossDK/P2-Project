b81170aefd941bd332a67a745c15bc1816f7aa92
PeterDaneholm <av28qm@student.aau.dk>
2022-05-04 13:34:45 +0200

    Update

--- Assets/Peter Folder/Scripts/TimerSpaceDifference.cs
+++ Assets/Peter Folder/Scripts/TimerSpaceDifference.cs
@@ -5,28 +5,12 @@ using UnityEngine;
 public class TimerSpaceDifference : MonoBehaviour
 {
 
-    [SerializeField] GameObject Body1, Body2, Body3, Body4;
-    public GameObject WakeUp, Sleep, ComingHome, LeavingHome;
 
     // Update is called once per frame
     void Update()
     {
         //CheckIfActive();
-        while (Body1 != null)
-        {
-            Sleep.transform.position = new Vector3(16f, -30f, 0f);
-            ComingHome.transform.position = new Vector3(16f, -45f, 0f);
-            LeavingHome.transform.position = new Vector3(16f, -60f, 0f);
-        }
-        while (Body2 != null)
-        {
-            ComingHome.transform.position = new Vector3(16f, -45f, 0f);
-            LeavingHome.transform.position = new Vector3(16f, -60f, 0f);
-        }
-        while (Body3 != null)
-        {
-            LeavingHome.transform.position = new Vector3(16f, -60f, 0f);
-        }
+ 
     }
 
 
