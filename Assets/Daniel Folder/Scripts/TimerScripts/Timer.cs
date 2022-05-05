9da8b1b08da1a13bcb13ef3bbacaae009d353e6e
MiniBossDK <palovt@protonmail.com>
2022-05-05 12:37:53 +0200

    WIP: TimerPage save data

--- Assets/Daniel Folder/Scripts/TimerScripts/Timer.cs
+++ Assets/Daniel Folder/Scripts/TimerScripts/Timer.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 [Serializable]
 public struct Timer
 {
-    public Timer(int startTime, int endTime)
+    public Timer(Guid id, int startTime, int endTime)
     {
+        ID = id.ToString();
         StartTime = startTime;
         EndTime = endTime;
     }
+    public string ID;
+
+    public string Name;
+
+    public TimerType type;
 
     public int StartTime;
 
     public int EndTime;
-    
+
+    public WeekDay[] weekdays;
+
+    public Room[] EnabledRooms;
+
 }
